8afc23b2a3a1d7726e50811c87d7921c9f38dbba
lzh107u <52312001+lzh107u@users.noreply.github.com>
2022-11-04 17:31:15 +0800

    11-04-update

--- register/giwpage/views.py
+++ register/giwpage/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-
+import time
+from datetime import datetime
 # Create your views here.
 
+def timer():
+    seconds = time.time()
+    local_time = time.ctime( seconds )
+    time_struct = time.localtime( seconds )
+    
+    if time_struct.tm_mday >= 8:
+        # after 11/8, the stage changes into 'standard rate'
+        return True
+    else:
+        return False
+    
 
 def render_index( request ):
-    print( 'view, render_index: called.' )
-    
-    return render( request = request, template_name = "giwpage/index2.html" )
+    ret = timer()
+    if ret is True:
+        return render( request = request, template_name = "giwpage/index3.html" )
+    else:
+        return render( request = request, template_name = 'giwpage/index2.html' )
